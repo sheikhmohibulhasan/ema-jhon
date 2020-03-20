a43259d1d10514edc98bee153d7400863337ee66
sheikhmohibulhasan <sheikhhasan24@gmail.com>
2020-03-21 03:44:34 +0600

    ema-jhon completed

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Sorry, Page Not Found</h1>
+            <h3>404 Error!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ReviewItem.css';
+
+const ReviewItem = (props) => {
+   
+    const {name, quantity, key, price} = props.product;
+    return (
+        <div className="review-item">
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity: {quantity}</p>
+    <p><small>$ {price}</small></p>
+            <br/>
+            <button className="main-button"
+                onClick = {() => props.removeProduct(key)}
+            >Remove Item</button>
+        </div>
+    );
+};
+
+export default ReviewItem;
